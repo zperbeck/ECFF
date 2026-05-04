daacc90fd9e2861c084754ecbe1ad33a150bac8e
zperbeck <zperbeck@gmail.com>
2026-05-03 21:29:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+body {
+font-family: Arial;
+text-align: center;
+background: #0b1c2c;
+color: white;
+}
 
+nav button {
+margin: 10px;
+padding: 10px;
+}
+
+.page {
+display: none;
+}
+
+#messages {
+height: 200px;
+overflow-y: scroll;
+border: 1px solid white;
+margin: 10px;
+}
